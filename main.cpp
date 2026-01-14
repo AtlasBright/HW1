57750228c320cc5cd64b22b18031cbd0dbcac656
AtlasBright <brightjackscp@gmail.com>
2026-01-14 09:11:37 +0300

    Udp HW1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello\n";
-    std::cout << "My name is Yan\n";
-    std::cout << "I'm "<< 20 <<" years old.";
+    std::cout << "Yan\n";
+    std::cout << 25 ;
         return 0;
 }
